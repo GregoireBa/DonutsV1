61e176fd105c0f8d0f21f533df2c02c6b36491bf
Buhari yavuz <buhari.yavuz@gmail.com>
2022-04-22 16:46:28 +0200

    test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,7 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
+        //commentaire testes
         return $this->render('index.html.twig', [
             'controller_name' => 'MainController',
         ]);
