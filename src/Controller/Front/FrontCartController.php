9a8023951411b252d594d212f7103676b5ab152c
Buhariy <buhari.yavuz@gmail.com>
2022-04-29 11:11:20 +0200

    panier pas finis

--- src/Controller/Front/FrontCartController.php
+++ src/Controller/Front/FrontCartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -9,18 +10,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 
 /**
- * @Route("/card", name="card_")
+ * @Route("/cart", name="cart_")
  */
 class FrontCartController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
-    {
-        return $this->render('front_cart/index.html.twig', [
-            'controller_name' => 'FrontCartController',
-        ]);
+    public function index(SessionInterface $session, ProduitRepository $produitRepository)
+    {   
+        $panier= $session->get("panier",[]);
+
+        // On "fabrique" les données
+        $dataPanier[] = [];
+        $total = 0;
+
+        foreach($panier as $id => $quantite){
+            $produit = $produitRepository->find($id);
+            $dataPanier = [
+                "produit" => $produit,
+                "quantite" => $quantite
+            ];
+            $total += $produit->getPrix()*$quantite; 
+        }
+
+        return $this->render('front/cart_user.html.twig', ["dataPanier" => $dataPanier,"total" =>$total]);
     }
 
     /**
@@ -39,7 +53,7 @@ class FrontCartController extends AbstractController
 
         //save dans la sesion 
         $session->set("panier",$panier);
-
-        dd($session);
+        
+        return $this->redirectToRoute("cart_index");
     }
 }
